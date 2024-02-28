0c7dc52d16c808504ea85b4876927e040d045107
LaFii <real221nama331@gmail.com>
2024-02-28 23:00:52 +0300

    update database script

--- migrations/create_database.sql
+++ migrations/create_database.sql
@@ -12,12 +12,18 @@ CREATE TYPE tp_application_status AS ENUM (
     'Обработано',
     'Отказано'
     );
+CREATE TYPE tp_contract_relationship AS ENUM (
+    'Прохождение произв практики',
+    'Трудоустройство'
+    );
 
 CREATE TABLE tbl_applications
 (
     application_id        SERIAL PRIMARY KEY,
     date_submitted        DATE                  NOT NULL,
     applicant_name        VARCHAR(255)          NOT NULL,
+    applicant_surname     VARCHAR(255)          NOT NULL,
+    applicant_middlename  VARCHAR(255)          NOT NULL,
     education_institution VARCHAR(255),
     specialization        VARCHAR(255),
     status                tp_application_status NOT NULL default ('В процессе')
@@ -26,8 +32,9 @@ CREATE TABLE tbl_applications
 CREATE TABLE tbl_contracts
 (
     contract_id    SERIAL PRIMARY KEY,
-    start_date     DATE NOT NULL,
+    start_date     DATE                     NOT NULL,
     end_date       DATE,
+    relationship   tp_contract_relationship NOT NULL default ('Прохождение произв практики'),
     application_id INTEGER REFERENCES tbl_applications (application_id)
 );
 
