51d6b421e1329c5a7c15e05c4905917afd3fa85d
LaFii <real221nama331@gmail.com>
2024-06-10 15:28:58 +0300

    migration fix

--- scheme/migrations/000008_create_students_table.sql
+++ scheme/migrations/000008_create_students_table.sql
@@ -9,7 +9,7 @@ create table tbl_students
     student_name                 varchar(255) NOT NULL,
     student_middle_name          varchar(255) NOT NULL,
     student_last_name            varchar(255) NOT NULL,
-    student_phone_number         varchar(255) NOT NULL UNIQUE,
+    student_phone_number         varchar(255) NOT NULL,
 
     CONSTRAINT fk_specializations FOREIGN KEY (specialization_id) REFERENCES tbl_specializations (specialization_id) ON DELETE set null,
     CONSTRAINT fk_educational_establishment FOREIGN KEY (educational_establishment_id) REFERENCES tbl_educational_establishments (educational_establishment_id) ON DELETE set null
